2be208db6754ee78366fa874ecf886a68eebe717
littleeasy <b94209002@gmail.com>
2016-12-16 17:03:32 -0500

    Found bug when u = u(y)
    the flow stuck

--- fixed_velocity2.m
+++ fixed_velocity2.m
@@ -1,4 +1,7 @@
 function [u v] = fixed_velocity2(xx,yy,u0,v0)
 
-u = -2*u0*cos(2*pi*xx).*sin(2*pi*yy);
-v = 2*v0*sin(2*pi*xx).*cos(2*pi*yy);
+%u = u0*cos(2*pi*(xx-yy+.5));
+%v = v0*cos(2*pi*(xx-yy+.5));
+
+u = u0*cos(2*pi*(yy+.5));
+v = v0*cos(2*pi*(yy+.5));
